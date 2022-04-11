d3b8c0ec6776c183caf04eed4e98658ee0ac3f3d
sumin <lsm001005@naver.com>
2022-04-11 22:37:28 +0900

    Create queries with arguments

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -3,7 +3,7 @@ import { getById, people } from "./db.js";
 const resolvers = {
     Query: {
         people: () => people,
-        person: () => getById()
+        person: (_, { id }) => getById(id)
     }
 };
 
