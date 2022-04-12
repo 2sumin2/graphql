70a4a7572db36d8cd131099ae0243666adc87219
sumin <lsm001005@naver.com>
2022-04-12 20:16:35 +0900

    Separate typeDefs

---
+++ graphql/typeDefs.js
@@ -0,0 +1,22 @@
+const typeDefs = `
+    type User {
+        id: Int!,
+        email: String!,
+        statement: String!,
+        username: String!,
+        password: String!
+    }
+    type Query {
+        users: [User]!
+    }
+    type Mutation{
+        addUser(
+            email: String!,
+            statement: String!,
+            username: String!,
+            password: String!
+        ): User!,
+        deleteUser(username: String!): Boolean!
+}`;
+
+export default typeDefs;
--- index.js
+++ index.js
@@ -1,31 +1,14 @@
 import { createServer } from '@graphql-yoga/node';
 import resolvers from './graphql/resolvers.js';
+import typeDefs from './graphql/typeDefs.js';
 
 // Create your server
+
 const server = createServer({
     schema: {
-        typeDefs:
-            `type User {
-            id: Int!,
-            email: String!,
-            statement: String!,
-            username: String!,
-            password: String!
-        } 
-        type Query {
-            users: [User]!
-        }
-        type Mutation{
-            addUser(email: String!,
-                statement: String!,
-                username: String!,
-                password: String!
-            ) : User!,
-            deleteUser(username:String!): Boolean!
-        }`,
+        typeDefs,
         resolvers
     }
-
 });
 // start the server and explore http://localhost:4000/graphql
 server.start(); 
